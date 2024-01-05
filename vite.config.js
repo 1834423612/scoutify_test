9a4c9427d1b1f511714a36263d368e693f1b78ea
1834423612 <1834423612@qq.com>
2024-01-04 20:26:09 -0500

    test: add base in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'./'
   // resolve: {
   //   alias: {
   //     '@': path.resolve(__dirname, './src') // 将 @ 别名指向 src 目录的绝对路径
